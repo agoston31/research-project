8669d0e637d79f8655d609874daab2b39d79e828
Ágoston Szabó <Szabo-2@student.tudelft.nl>
2026-05-20 18:27:10 +0200

    Added experimental setup

---
+++ run_experiments.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set OUTDIR=results\articulation
+
+if not exist %OUTDIR% mkdir %OUTDIR%
+
+for /R tests\instances %%f in (*.dzn) do (
+    set "folder=%%~dpf"
+    for %%a in ("%%~dpf.") do set "parent=%%~nxa"
+
+    echo Running %%f
+
+    minizinc --solver pumpkin --statistics ^
+        tests\models\circuit_model.mzn ^
+        "%%f" ^
+        > "%OUTDIR%\!parent!_%%~nf.txt"
+)
